642a04c5d309a9d5f3d51e134726437a759e0d4d
Dion Ricky Saputra <dionrickysptr@gmail.com>
2021-06-12 06:22:43 +0700

    Update app theme

--- app/src/main/java/com/example/plantalk_test2704/PlantalkConfig.kt
+++ app/src/main/java/com/example/plantalk_test2704/PlantalkConfig.kt
@@ -3,6 +3,7 @@ package com.example.plantalk_test2704
 class PlantalkConfig {
     companion object {
 //        val BASE_URL = "https://dev.plantalk.dionricky.com:8081/"
-        val BASE_URL = "https://plantalk-test2704--staging-23jncdv5.web.app/"
+//        val BASE_URL = "https://plantalk-test2704--staging-23jncdv5.web.app/"
+        val BASE_URL = "https://plantalk-test2704.web.app/"
     }
 }
